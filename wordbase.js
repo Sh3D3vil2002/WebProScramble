cd92dc40f6d1090df69439670b81db048a9a2c24
Jake88w <124826026+Jake88w@users.noreply.github.com>
2023-04-01 14:20:04 -0400

    Update wordbase.js

--- wordbase.js
+++ wordbase.js
@@ -1,12 +1,40 @@
 const words = [
 
-	{	word: ""
-		hint: ""
+	{	word: "salt"
+		hint: "a white crystaline subsance present in sea water"
 	
 											},
 											
-	{	word: ""
-		hint: ""
+	{	word: "pepper"
+		hint: "a hot tasting powder used to season food"
+	
+											},
+	{	word: "apple",
+		hint: "the fuit of a tree from the rose family, usually red and green"
+	
+											},
+	{	word: "peach",
+		hint: "a round juicy stone fruit"
+	
+											},
+	{	word: "wood",
+		hint: "a hard fibrous material obtained from a plant"
+	
+											},
+	{	word: "pen",
+		hint: "a dwaring instrument"
+	
+											},
+	{	word: "cloths",
+		hint: "fabric used to cover a persons body"
+	
+											},
+	{	word: "brick",
+		hint: "a hard stone used in the making of houses"
+	
+											},
+	{	word: "paper",
+		hint: "a fibrous material used for drawing and writing"
 	
 											},
 
